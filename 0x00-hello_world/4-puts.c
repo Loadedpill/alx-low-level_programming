36ed3ea50b923c30ca5669210ac3d545064531eb
Promise Igwe <lcmictcenter@gmail.com>
2022-09-09 05:04:59 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,7 @@
  * this program prints "programming is like building a multilingual puzzle"
  * Return: 0
  */
+int main()
 {
 		puts("\"programming is like building a multilingual puzzle");
 			return (0);

98cc24713deea4a69d04c7816bd5747823ea9d7c
Promise Igwe <lcmictcenter@gmail.com>
2022-09-09 05:01:47 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
-
 /**
- * <<<<<<< HEAD
- *  * main	-print the phrase "Programming is like building a multilingual puzzle
- *   * return (0) success
- *    */
-int main (void)
-{ 
-		puts("/"Programming is like building a multilingual puzzle");
-			ruturn (0);
-			 * main - main block
-				 *  * Return: 0
-				 *  =======
-				 *   * main - prints the phrase "Programming is like building a multilingual puzzle
-				 *    * Return: 0 if exited properly, non-zero otherwise
-				 *    >>>>>>> 68209a233b53e313f3b5278f6fbef08cf3edbf7b
-				 *     */
-				 int main(void)
-				 {
-					 	puts("\"Programming is like building a multilingual puzzle");
-							return (0);
-				 }{
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+{
+		puts("\"programming is like building a multilingual puzzle");
+			return (0);
 }

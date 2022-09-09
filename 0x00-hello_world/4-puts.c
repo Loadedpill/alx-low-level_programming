7caf0ca2a07cfa926749b7fe8bf57176bf3fe892
Promise Igwe <lcmictcenter@gmail.com>
2022-09-09 04:00:58 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,5 @@
 #include <stdio.h>
-int puts("Programming is like building a multilingual puzzle\n");
-return 0;
+{
+int puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}

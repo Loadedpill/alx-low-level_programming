070e95a06bae936fd4718af040c2101b6eb3c977
Promise Igwe <lcmictcenter@gmail.com>
2022-09-10 09:31:05 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("Last digit of", n "is %d and is greater than 5\n");
+printf("Last digit of %d is %d and is greater than 5\n");
 }
 else if (n == 0)
 {
-printf("Last digit of", n "is %d and is 0\n");
+printf("Last digit of %d is %d and is 0\n");
 }
 else if (n < 6 && !0)
 {

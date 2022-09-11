5f449431e5a0b99ddf51076f045f18d7e8fc852f
Promise Igwe <lcmictcenter@gmail.com>
2022-09-10 18:05:39 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,16 +8,15 @@
 int main(void)
 {
 int ch;
-for (ch = 'a'; ch <= 'Z' ch++)
+for (ch = 'a'; ch <= 'z' ch++)
 {
 if (ch !='e' && ch !='q')
 {
 putchar(ch);
+else
 {
-	else
 }
 }
-
 }
 putchar('\n');
 return (0);

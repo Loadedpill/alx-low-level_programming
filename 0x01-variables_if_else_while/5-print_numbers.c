e0aa32afc8b585af51eb1ea8b787707f6d17ae9c
Promise Igwe <lcmictcenter@gmail.com>
2022-09-10 18:42:30 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry point
+ * Description: prints allsingle digit numbers of
+ * base 10 from 0
+ * Return: Always 0 (success)
+ **/
+int main(void)
+{
+int b;
+for (b = 0; b < 10; b++)
+{
+printf("%d", n);
+}
+putchar('\n');
+return (0);
+}

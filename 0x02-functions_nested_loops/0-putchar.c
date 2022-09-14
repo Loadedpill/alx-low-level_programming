a173fb66c78af7eafcd785fd2ae80354e08563ad
Promise Igwe <lcmictcenter@gmail.com>
2022-09-14 05:52:54 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
+ **/
+int main(void)
+{
+_putchar('_');
+_putchar("putchar");
+return (0);
+
+}

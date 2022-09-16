534584658cb3c23b256d98f261d60554a175ab15
Promise Igwe <lcmictcenter@gmail.com>
2022-09-16 14:02:27 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,14 @@ void more_numbers(void)
 
 	for (row = 1; row <= 10; row++)
 	{
-	for (number = 0; number <= 14; number++)
-	{
-		tens = number / 10;
-												units = number % 10;
-											if (number > 9)
-		_putchar(tens + '0');
+		for (number = 0; number <= 14; number++)
+		{
+			tens = number / 10;								units = number % 10;
+			if (number > 9)
+				_putchar(tens + '0');
 
-	_putchar(units + '0');
-	}
+			_putchar(units + '0');
+		}
 		_putchar('\n');
 											}
 }

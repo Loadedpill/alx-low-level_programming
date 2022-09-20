d7905dd088bbbece8cf65a280c4cf703a25de29d
Promise Igwe <lcmictcenter@gmail.com>
2022-09-20 11:02:02 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _puts - prints a string in reverse
+ *  @str: pointer parameter
+ *    */
+void _puts(char *str)
+{
+int a = 0;
+
+while (str[a] != '\0')
+{
+_putchar(str[i]);
+i--;
+}
+_putchar('\n');
+}

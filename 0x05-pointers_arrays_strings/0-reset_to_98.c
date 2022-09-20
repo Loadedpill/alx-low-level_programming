e95d5ffdafecacced248c9f746e3c6c87b78955e
Promise Igwe <lcmictcenter@gmail.com>
2022-09-20 05:52:28 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 it reset to 98
- * @n:checked
- * Return: Always 0.
+ * Reset_to_98
+ * @n: changing the value of the variable
+ * return 0:
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 *n = 98;
 }

a0da7db1ad79d10204d41798b79f828b281460ff
Promise Igwe <lcmictcenter@gmail.com>
2022-09-20 19:11:28 -0700

    stack H.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,33 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _atoi - converts a string to an int
- * @s: the string to be changed
- *
- * Return: the converted int
+ * _atoi - This function converts a string to an integer.
+ * @s: Pointer parameter
+ * Return: returns an integer value
  */
-
 int _atoi(char *s)
 {
-int sign = 1;
-unsigned int num = 0;
-do {
-if (*s == '-')
-sign *= -1;
-else if (*s >= '0' && *s <= '9')
-num = num * 10 + (*s - '0');
-else if (num > 0)
+unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
+
+while (*(s + count) != '\0')
+{
+if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 break;
-while (*s++);
-return (num * sign);
+if (*(s + count) == '-')
+										k *= -1;
+if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+{
+if (size > 0)
+m *= 10;
+size++;
+}
+count++;
+}
+for (i = count - size; i < count; i++)
+{
+j = j + ((*(s + i) - 48) * m);
+m /= 10;
+}
+return (j * k);
 }

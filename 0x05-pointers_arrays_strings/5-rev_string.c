da84433ee621470f80083d39d9dd928cc41dbe36
Promise Igwe <lcmictcenter@gmail.com>
2022-09-20 17:43:14 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include <stdio,h>
+#include "main.h"
+
+/**
+ *  resverses_string - This function reverses a string
+ *  @s: pointer parameter
+ */
+void print_rev(char *s)
+{
+int i = 0;
+int j;
+
+while (s[i] != '\0')
+{
+i++;
+}
+for (j = i - 1; j >= 0; j--)
+}
+

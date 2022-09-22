104e16a01e4d27fcc735837b267a1e22756beb39
Promise Igwe <lcmictcenter@gmail.com>
2022-09-22 15:07:40 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,21 +10,7 @@
  */
 void print_number(int n)
 {
-unsigned int num;
-
-/*check if number is negative*/
-
-num = n;
-
-if (n < 0)
-{
-_putchar(45);
-num = -n;
-}
-/* print number by recursion*/
-if (num / 10)
-{
-print_number(num / 10);
-}
-_putchar((num % 10) + '0');
+n = 10;
+_putchar(n);
+return(0);
 }

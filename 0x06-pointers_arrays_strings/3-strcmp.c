f2b1f3f8ae057bb609a1247c1d608b3066f5b08a
Promise Igwe <lcmictcenter@gmail.com>
2022-09-22 07:14:17 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * _strcmp- copies the string pointed to by src,
- *
  * @s1: checked
  * @s2:checked
  * Return: int
@@ -10,7 +9,6 @@
 int _strcmp(char *s1, char  *s2)
 {
 int i, output;
-
 i = 0;
 while (*(s1 + i) == *(s2 + i) && *(s2 + i) && *(s1 + i) != '\0')
 i++;
